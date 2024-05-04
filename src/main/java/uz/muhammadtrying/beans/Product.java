6f7bcccfe7a587e25979ee559aed7cd5cafd3e57
Muhammad <muhammadtrying@gmail.com>
2024-05-04 08:37:29 +0500

    initial commit

--- src/main/java/uz/muhammadtrying/beans/Product.java
+++ src/main/java/uz/muhammadtrying/beans/Product.java
@@ -1,8 +1,5 @@
 package uz.muhammadtrying.beans;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Product {
     public void save() {
         System.out.println("saving product...");
--- src/main/java/uz/muhammadtrying/config/MyConfig.java
+++ src/main/java/uz/muhammadtrying/config/MyConfig.java
@@ -1,7 +1,13 @@
 package uz.muhammadtrying.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uz.muhammadtrying.beans.Product;
 
-@ComponentScan("uz.muhammadtrying")
+@Configuration
 public class MyConfig {
+    @Bean
+    public Product product() {
+        return new Product();
+    }
 }
